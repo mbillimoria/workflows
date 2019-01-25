25e93eb3826f1241abc21b730e61af0692d89179
Michael Billimoria <mbillimoria@dxc.com>
2019-01-25 21:25:50 +1100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
-gulp.task('coffee', function() {
-    gulp.src('components/coffee/tagline.coffee')
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', async function() {
+    gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
          .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
